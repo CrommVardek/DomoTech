f1b73f776ddd438b602259f290546d90fd538f4a
Dorian Lecomte <lecomte.dorian@gmail.com>
2016-04-14 16:05:40 +0200

    Ajout de documents pour la base de données premiere partie

---
+++ docs/DB/DataSetRoomInitialisation.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM laboIAM.room
+LIMIT 0, 1000
+
+-- Date: 2016-04-13 11:41
+*/
+INSERT INTO `room` (`roomId`,`name`,`description`) VALUES (1,'Cuisine','La cuisine est une pièce spécifique dans un bâtiment, spécialement équipée pour la préparation des aliments et des plats. On y dispose généralement de l\'eau courante et divers appareils électroménagers (cuisinière, four, réfrigérateur...). La cuisine peut disposer de locaux annexes : les celliers et fruitiers des habitations, les chambres froides de restaurants, d\'hôtels et d\'usines de cuisine industrielle. Dans un logement de type studio la cuisine n\'est pas une pièce mais un équipement le long d\'un mur.');
---
+++ docs/DB/DataSetSpicesBoxInitialisation.sql
@@ -0,0 +1,13 @@
+/*
+Data set pour l'initialisation de la roue à épices. 
+Chacun des 6 emplacements sont vides.
+Codé par Dorian Lecomte.
+
+-- Date: 2016-04-13 11:40
+*/
+INSERT INTO `spiceBox` (`location`,`spiceId`) VALUES (1,NULL);
+INSERT INTO `spiceBox` (`location`,`spiceId`) VALUES (2,NULL);
+INSERT INTO `spiceBox` (`location`,`spiceId`) VALUES (3,NULL);
+INSERT INTO `spiceBox` (`location`,`spiceId`) VALUES (4,NULL);
+INSERT INTO `spiceBox` (`location`,`spiceId`) VALUES (5,NULL);
+INSERT INTO `spiceBox` (`location`,`spiceId`) VALUES (6,NULL);
